1d55b8ad626e30594fa493a19c2d76183fcf7769
andrii.kucheruk.l@gmail.com <Wodoley7227>
2019-05-16 01:43:16 +0300

    Logo change

--- scripts/RouterService.js
+++ scripts/RouterService.js
@@ -34,7 +34,7 @@ window.onload =  () => {
 
     let currentPath = window.location.pathname;
 
-    switch ('/events') {
+    switch ('/') {
         case '/': {
             dynamicView.load("pages/home-page/HomePage.html");
             break;
